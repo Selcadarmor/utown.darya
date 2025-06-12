3b9da7d641be7df1d5abc272e4afcfa6569c7f37
Ten Valeriy <walera.tenlist.ru@MacBook-Air-Ten.local>
2025-06-12 23:58:21 +0900

    intial commit

---
+++ src/main/java/com/example/demo/model/Role.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+
+
+
+}
