db6f3d9eac76c6c01faa14ecf0cb70fb5780833a
Ten Valeriy <walera.tenlist.ru@MacBook-Air-Ten.local>
2025-06-15 04:07:45 +0900

    JWT

--- src/main/java/com/example/demo/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.dto.RegisterRequest;
-
-public interface UserService {
-    boolean existsByUsername(String username);
-    void saveUser(RegisterRequest request);
-}
