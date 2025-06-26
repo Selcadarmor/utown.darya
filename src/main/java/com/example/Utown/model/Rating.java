ef389e67b419df62bc90475b85c9c3c828409788
Darya Kim <daryakim@MacBook-Air-Darya.local>
2025-06-26 16:28:23 +0900

    Merge kim into ten

--- src/main/java/com/example/Utown/model/Rating.java
+++ src/main/java/com/example/Utown/model/Rating.java
@@ -1,15 +1,11 @@
 package com.example.Utown.model;
 
-import com.example.Utown.model.enumFiles.RatingStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
 import java.time.LocalDateTime;
 
 @Entity
@@ -24,8 +20,6 @@ public class Rating {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Enumerated(EnumType.STRING)
-    private RatingStatus status;
     @ManyToOne
     @JoinColumn(name = "restaurant_id")
     private Restaurant restaurant;
--- src/main/java/com/example/Utown/service/UserType/restaurantAdmin/RestaurantAdminService.java
+++ src/main/java/com/example/Utown/service/UserType/restaurantAdmin/RestaurantAdminService.java
@@ -1,18 +1,18 @@
-package com.example.Utown.service.UserType.restaurantAdmin;
+//package com.example.Utown.service.UserType.restaurantAdmin;
 
 
-import com.example.Utown.dto.restaurantAdmin.RestaurantAdminCreateDto;
-import com.example.Utown.dto.restaurantAdmin.RestaurantAdminDto;
-import com.example.Utown.dto.restaurantAdmin.RestaurantAdminUpdateDto;
+//mport com.example.Utown.dto.restaurantAdmin.RestaurantAdminCreateDto;
+//import com.example.Utown.dto.restaurantAdmin.RestaurantAdminDto;
+//import com.example.Utown.dto.restaurantAdmin.RestaurantAdminUpdateDto;
 
-import java.util.List;
+//import java.util.List;
 
-public interface RestaurantAdminService {
+//public interface RestaurantAdminService {
     /// администратор рестаранов
-    RestaurantAdminDto createAdmin(RestaurantAdminCreateDto dto);
-    List<RestaurantAdminDto> getAllRestaurantAdmins();
-    RestaurantAdminDto getRestaurantAdminById(Long id);
-    RestaurantAdminDto updateRestaurantAdmin(Long id, RestaurantAdminUpdateDto dto);
-    void deleteRestaurantAdmin(Long id);
-}
+   // RestaurantAdminDto createAdmin(RestaurantAdminCreateDto dto);
+   // List<RestaurantAdminDto> getAllRestaurantAdmins();
+   // RestaurantAdminDto getRestaurantAdminById(Long id);
+   // RestaurantAdminDto updateRestaurantAdmin(Long id, RestaurantAdminUpdateDto dto);
+   // void deleteRestaurantAdmin(Long id);
+//}
 
