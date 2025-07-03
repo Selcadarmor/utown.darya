e2ceaec1e003d86c3f40ec4a0c39e078643b401a
Ten Valeriy <walera.tenlist.ru@MacBook-Air-Ten.local>
2025-07-04 03:56:30 +0900

    update

--- src/main/java/com/example/Utown/service/UserType/restaurant/RestaurantService.java
+++ src/main/java/com/example/Utown/service/UserType/restaurant/RestaurantService.java
@@ -3,7 +3,7 @@ package com.example.Utown.service.UserType.restaurant;
 import com.example.Utown.dto.adminDto.RestaurantCreateUpdateDto;
 import com.example.Utown.dto.adminDto.RestaurantDetailsDto;
 import com.example.Utown.dto.adminDto.RestaurantInfoDto;
-import com.example.Utown.dto.clientDto.RestaurantDto;
+import com.example.Utown.dto.clientDto.RestaurantForClientDto;
 
 import java.util.List;
 
@@ -13,7 +13,7 @@ public interface RestaurantService {
     RestaurantDetailsDto createRestaurant(RestaurantCreateUpdateDto dto);
     RestaurantDetailsDto updateRestaurant(Long id, RestaurantCreateUpdateDto dto);
     void deleteRestaurant(Long id);
-    List<RestaurantDto> getAllForClientRestaurants();
-    List<RestaurantDto> getRestaurantsSortedByFastestDelivery();
+    List<RestaurantForClientDto> getAllRestaurantsForClientSortedByDeliveryTime();
+    List<RestaurantForClientDto> getAllRestaurantsForClient();
     long countRestaurantsByCategory(Long categoryId);
 }
