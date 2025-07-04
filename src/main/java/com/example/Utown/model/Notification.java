d59183fe3c25633da6a536d5be9f58b0c91db6f4
Ten Valeriy <walera.tenlist.ru@MacBook-Air-Ten.local>
2025-07-05 03:00:25 +0900

    +

--- src/main/java/com/example/Utown/model/Notification.java
+++ src/main/java/com/example/Utown/model/Notification.java
@@ -71,5 +71,9 @@ public class Notification {
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
 
 }
