219b10764c49e54fc4cdd2b1a9106d0b9ef00e75
Darya Kim <daryakim@MacBook-Air-Darya.local>
2025-07-08 10:51:25 +0900

    Edit Dto and Mappers

--- src/main/java/com/example/Utown/mapper/DishCategoryMapper.java
+++ src/main/java/com/example/Utown/mapper/DishCategoryMapper.java
@@ -1,11 +1,13 @@
 package com.example.Utown.mapper;
 
 
+import com.example.Utown.dto.dishCategoryDTO.DishCategoryCreateDto;
 import com.example.Utown.dto.dishCategoryDTO.DishCategoryDto;
 import com.example.Utown.dto.dishCategoryDTO.DishCategoryDetailsDto;
 import com.example.Utown.model.DishCategory;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface DishCategoryMapper {
@@ -22,5 +24,14 @@ public interface DishCategoryMapper {
     @Mapping(source = "restaurant.id", target = "restaurantId")
     DishCategoryDetailsDto toCreateDto(DishCategory category);
 
+    @Mappings({
+            @Mapping(target = "isActive", constant = "true"),
+            @Mapping(target = "createdAt", ignore = true),
+            @Mapping(target = "updatedAt", ignore = true),
+            @Mapping(target = "restaurant", ignore = true),
+            @Mapping(target = "file", ignore = true),
+            @Mapping(target = "dishes", ignore = true)
+    })
+    DishCategory toEntity(DishCategoryCreateDto dto);
 
 }
--- src/main/java/com/example/Utown/mapper/OrderInfoMapper.java
+++ src/main/java/com/example/Utown/mapper/OrderInfoMapper.java
@@ -14,16 +14,79 @@ public interface OrderInfoMapper {
 
 
     @Mappings({
+            @Mapping(target = "deliveryTime", ignore = true),
+            @Mapping(target = "details", ignore = true),
+            @Mapping(target = "fullAddress", ignore = true),
+            @Mapping(target = "isPaid", ignore = true),
+            @Mapping(target = "latitude", ignore = true),
+            @Mapping(target = "longitude", ignore = true),
+            @Mapping(target = "noteForCourier", ignore = true),
+            @Mapping(target = "number", ignore = true),
+            @Mapping(target = "orderPrice", ignore = true),
+            @Mapping(target = "payment", ignore = true),
+            @Mapping(target = "postcode", ignore = true),
+            @Mapping(target = "restaurantPhone", ignore = true),
+            @Mapping(target = "state", ignore = true),
+            @Mapping(target = "status", ignore = true),
+            @Mapping(target = "street", ignore = true),
+            @Mapping(target = "time", ignore = true),
+            @Mapping(target = "timeOfAccepted", ignore = true),
+            @Mapping(target = "timeOfDelivery", ignore = true),
+            @Mapping(target = "timeOfSending", ignore = true),
+            @Mapping(target = "totalSum", ignore = true),
+            @Mapping(target = "typeAddress", ignore = true),
+            @Mapping(target = "cookingTime", ignore = true),
+            @Mapping(target = "deliveryStatus", ignore = true),
+            @Mapping(target = "endTimeOfCooking", ignore = true),
+            @Mapping(target = "intercomCode", ignore = true),
+            @Mapping(target = "createdAt", ignore = true),
+            @Mapping(target = "updatedAt", ignore = true),
             @Mapping(target = "city", ignore = true),
             @Mapping(target = "clientPhone", ignore = true),
             @Mapping(target = "date", ignore = true),
             @Mapping(target = "deliveryPrice", ignore = true),
-            @Mapping(target = "dishesToOrder", ignore = true),
             @Mapping(target = "restaurant", ignore = true),
-            @Mapping(target = "client", ignore = true)
-})
+            @Mapping(target = "client", ignore = true),
+            @Mapping(target = "dishesToOrder", ignore = true),
+    })
     Order toEntity(OrderInfoDto dto);
 
-    @Mapping(target = "dishesToOrder", ignore = true)
+
+    @Mappings({
+            @Mapping(source = "restaurantDto", target = "restaurant"),
+            @Mapping(target = "city", ignore = true),
+            @Mapping(target = "clientPhone", ignore = true),
+            @Mapping(target = "date", ignore = true),
+            @Mapping(target = "deliveryPrice", ignore = true),
+            @Mapping(target = "deliveryTime", ignore = true),
+            @Mapping(target = "details", ignore = true),
+            @Mapping(target = "fullAddress", ignore = true),
+            @Mapping(target = "isPaid", ignore = true),
+            @Mapping(target = "latitude", ignore = true),
+            @Mapping(target = "longitude", ignore = true),
+            @Mapping(target = "noteForCourier", ignore = true),
+            @Mapping(target = "number", ignore = true),
+            @Mapping(target = "orderPrice", ignore = true),
+            @Mapping(target = "payment", ignore = true),
+            @Mapping(target = "postcode", ignore = true),
+            @Mapping(target = "restaurantPhone", ignore = true),
+            @Mapping(target = "state", ignore = true),
+            @Mapping(target = "status", ignore = true),
+            @Mapping(target = "street", ignore = true),
+            @Mapping(target = "time", ignore = true),
+            @Mapping(target = "timeOfAccepted", ignore = true),
+            @Mapping(target = "timeOfDelivery", ignore = true),
+            @Mapping(target = "timeOfSending", ignore = true),
+            @Mapping(target = "totalSum", ignore = true),
+            @Mapping(target = "typeAddress", ignore = true),
+            @Mapping(target = "cookingTime", ignore = true),
+            @Mapping(target = "deliveryStatus", ignore = true),
+            @Mapping(target = "endTimeOfCooking", ignore = true),
+            @Mapping(target = "intercomCode", ignore = true),
+            @Mapping(target = "createdAt", ignore = true),
+            @Mapping(target = "updatedAt", ignore = true),
+            @Mapping(target = "client", ignore = true),
+            @Mapping(target = "dishesToOrder", ignore = true)
+    })
     Order updateFromDto(OrderInfoDto orderInfoDto, @MappingTarget Order order);
 }
