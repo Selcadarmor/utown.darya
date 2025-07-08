2bf2a9301c98c613596ae404c0a5e69dd80b54d4
Darya Kim <daryakim@MacBook-Air-Darya.local>
2025-07-08 19:26:25 +0900

    Merge kim into ten

--- src/main/java/com/example/Utown/model/UserType/Client.java
+++ src/main/java/com/example/Utown/model/UserType/Client.java
@@ -32,7 +32,7 @@ public class Client extends User {
     )
     private Set<Restaurant> favoriteRestaurants;
 
-    @OneToMany(mappedBy = "client",  cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @OneToMany( cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JoinColumn(name = "client_id")
     private Set<Address> addresses;
 
@@ -40,7 +40,7 @@ public class Client extends User {
     @JoinColumn(name = "file_id")
     private FileInfo fileInfo;
 
-    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     @JoinColumn(name = "client_id")
     private List<Order> orders;
 
