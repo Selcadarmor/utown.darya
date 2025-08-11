5a6fa37c195cbd88ca166e3c556de8064f7f015e
Ten Valeriy <walera.tenlist.ru@MacBook-Air-Ten.local>
2025-08-12 07:25:30 +0900

    update

--- src/main/java/com/example/Utown/config/Utills/JWTProperties.java
+++ src/main/java/com/example/Utown/config/Utills/JWTProperties.java
@@ -3,7 +3,6 @@ package com.example.Utown.config.Utills;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
